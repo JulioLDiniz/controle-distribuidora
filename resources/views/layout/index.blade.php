e552b83bff565f893e463ecc86776ebb487b19f4
Julio Diniz <julio.dinizk1@gmail.com>
2019-02-01 10:11:14 -0200

    Finalizado para produção

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -116,8 +116,10 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ \Illuminate\Support\Facades\Auth::user()->name }}
                                         <i class="ti-arrow-circle-right"></i>
+                                        {{ __('Logout') }}
+
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- routes/web.php
+++ routes/web.php
@@ -10,54 +10,61 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 use App\Produto;
 use Illuminate\Http\Request;
 
-Route::get('/',function (){
-    return view('welcome');
-});
 
-Route::get('teste', function(){
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'DashboardController@index');
+
+
+    Route::get('teste', function () {
 //	echo date('d/m/Y \à\s H:i:s');
-    echo date("Y-m-d", time());
-});
-Route::get('/produtos', 'ProdutoController@index');
+        echo date("Y-m-d", time());
+    });
+    Route::get('/produtos', 'ProdutoController@index');
 
-Route::post('/cadastrar-produto', 'ProdutoController@cadastrar');
-Route::get('/cadastrar-produto', 'ProdutoController@indexCadastro');
+    Route::post('/cadastrar-produto', 'ProdutoController@cadastrar');
+    Route::get('/cadastrar-produto', 'ProdutoController@indexCadastro');
 
-Route::post('/alterar-produto', 'ProdutoController@alterar');
-Route::get('/alterar-produto-{id}', 'ProdutoController@indexAlteracao' );
+    Route::post('/alterar-produto', 'ProdutoController@alterar');
+    Route::get('/alterar-produto-{id}', 'ProdutoController@indexAlteracao');
 
-Route::post('/deletar-produto', 'ProdutoController@deletar');
-Route::get('/movimentacao-{id}', 'ProdutoController@indexMovimentacao');
-Route::post('/movimentacao-entrada', 'ProdutoController@movimentacaoEntrada');
+    Route::post('/deletar-produto', 'ProdutoController@deletar');
+    Route::get('/movimentacao-{id}', 'ProdutoController@indexMovimentacao');
+    Route::post('/movimentacao-entrada', 'ProdutoController@movimentacaoEntrada');
 
 
 //Caixa
-Route::get('/caixa', 'CaixaController@index');
-Route::get('/getProduto-{codigodebarras}', 'ProdutoController@produtoCodigoDeBarras');
+    Route::get('/caixa', 'CaixaController@index');
+    Route::get('/getProduto-{codigodebarras}', 'ProdutoController@produtoCodigoDeBarras');
 //Route::post('/movimentacao-saida','ProdutoController@movimentacaoSaida');
-Route::post('/venda','CaixaController@venda');
+    Route::post('/venda', 'CaixaController@venda');
 
 //Cliente
-Route::get('/clientes','ClienteController@index');
-Route::get('/cadastrar-cliente','ClienteController@indexCadastro');
-Route::post('/cadastrar-cliente', 'ClienteController@cadastrar');
-Route::post('/deletar-cliente', 'ClienteController@deletar');
-Route::get('/alterar-cliente-{id}', 'ClienteController@indexAlteracao' );
-Route::post('/alterar-cliente', 'ClienteController@alterar');
-Route::get('/historico-cliente-{id}', 'ClienteController@indexHistorico' );
-Route::post('/dar-baixa-saldo', 'ClienteController@darBaixaSaldo' );
+    Route::get('/clientes', 'ClienteController@index');
+    Route::get('/cadastrar-cliente', 'ClienteController@indexCadastro');
+    Route::post('/cadastrar-cliente', 'ClienteController@cadastrar');
+    Route::post('/deletar-cliente', 'ClienteController@deletar');
+    Route::get('/alterar-cliente-{id}', 'ClienteController@indexAlteracao');
+    Route::post('/alterar-cliente', 'ClienteController@alterar');
+    Route::get('/historico-cliente-{id}', 'ClienteController@indexHistorico');
+    Route::post('/dar-baixa-saldo', 'ClienteController@darBaixaSaldo');
 
 
-Route::get('dashboard', 'DashboardController@index');
+    Route::get('dashboard', 'DashboardController@index');
 
-Route::get('/vendas', function (Request $request) {
-    $vendas = new \App\Dashboard();
-    return $vendas->vendasDoDia($request->date);
+    Route::get('/vendas', function (Request $request) {
+        $vendas = new \App\Dashboard();
+        return $vendas->vendasDoDia($request->date);
+    });
+
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::get('/home', 'HomeController@index')->name('home');
